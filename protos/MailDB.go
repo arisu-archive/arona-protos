1e36e4d588ffc6a81b444980c602ff39ca021bbd
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2026-01-08 03:21:34 +0800

    refactor: incorrect map type for enum as key

--- protos/MailDB.go
+++ protos/MailDB.go
@@ -5,17 +5,17 @@ import (
 )
 
 type MailDB struct {
-	ServerId          int64                        `json:",omitempty,omitzero"`
-	AccountServerId   int64                        `json:",omitempty,omitzero"`
-	Type              flatdata.MailType            `json:",omitempty,omitzero"`
-	UniqueId          int64                        `json:",omitempty,omitzero"`
-	Sender            string                       `json:",omitempty,omitzero"`
-	LocalizedSender   map[flatdata.Language]string `json:",omitempty,omitzero"`
-	Comment           string                       `json:",omitempty,omitzero"`
-	LocalizedComment  map[flatdata.Language]string `json:",omitempty,omitzero"`
-	SendDate          MxTime                       `json:",omitempty,omitzero"`
-	ReceiptDate       *MxTime                      `json:",omitempty,omitzero"`
-	ExpireDate        *MxTime                      `json:",omitempty,omitzero"`
-	ParcelInfos       []ParcelInfo                 `json:",omitempty,omitzero"`
-	RemainParcelInfos []ParcelInfo                 `json:",omitempty,omitzero"`
+	ServerId          int64             `json:",omitempty,omitzero"`
+	AccountServerId   int64             `json:",omitempty,omitzero"`
+	Type              flatdata.MailType `json:",omitempty,omitzero"`
+	UniqueId          int64             `json:",omitempty,omitzero"`
+	Sender            string            `json:",omitempty,omitzero"`
+	LocalizedSender   map[string]string `json:",omitempty,omitzero"`
+	Comment           string            `json:",omitempty,omitzero"`
+	LocalizedComment  map[string]string `json:",omitempty,omitzero"`
+	SendDate          MxTime            `json:",omitempty,omitzero"`
+	ReceiptDate       *MxTime           `json:",omitempty,omitzero"`
+	ExpireDate        *MxTime           `json:",omitempty,omitzero"`
+	ParcelInfos       []ParcelInfo      `json:",omitempty,omitzero"`
+	RemainParcelInfos []ParcelInfo      `json:",omitempty,omitzero"`
 }
--- protos/ToastDB.go
+++ protos/ToastDB.go
@@ -1,16 +1,12 @@
 package protos
 
-import (
-	"github.com/arisu-archive/arona-flatbuffers/go/flatdata"
-)
-
 type ToastDB struct {
-	UniqueId     int64                        `json:",omitempty,omitzero"`
-	Text         string                       `json:",omitempty,omitzero"`
-	LocalizeText map[flatdata.Language]string `json:",omitempty,omitzero"`
-	ToastId      string                       `json:",omitempty,omitzero"`
-	BeginDate    MxTime                       `json:",omitempty,omitzero"`
-	EndDate      MxTime                       `json:",omitempty,omitzero"`
-	LifeTime     int32                        `json:",omitempty,omitzero"`
-	Delay        int32                        `json:",omitempty,omitzero"`
+	UniqueId     int64             `json:",omitempty,omitzero"`
+	Text         string            `json:",omitempty,omitzero"`
+	LocalizeText map[string]string `json:",omitempty,omitzero"`
+	ToastId      string            `json:",omitempty,omitzero"`
+	BeginDate    MxTime            `json:",omitempty,omitzero"`
+	EndDate      MxTime            `json:",omitempty,omitzero"`
+	LifeTime     int32             `json:",omitempty,omitzero"`
+	Delay        int32             `json:",omitempty,omitzero"`
 }
